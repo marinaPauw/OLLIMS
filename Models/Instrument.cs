9be3f098696636b7eca9abbc0500177b3fcc31fc
Marina Kriek <pauwmarina@gmail.com>
2020-08-18 11:40:01 +0200

    feat(AddInstrumentModel):Instrument model added

---
+++ Models/Instrument.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OLLIMS.Models
+{
+    public class Instrument
+    {
+        public string Name;
+        public double MeasurementrangeBottom;
+        public double MeasurementrangeTop;
+        public string Unit;
+        public string Vendor;
+        public int VerificationSchedule;
+        public int CalibrationSchedule;
+    }
+}
