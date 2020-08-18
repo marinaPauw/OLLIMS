910f0c162837efd75497380df5ab4c01caba25f9
Marina Kriek <pauwmarina@gmail.com>
2020-08-18 12:29:02 +0200

    feat(AddInstrToVerificationTests):InstrumentToVerificationTest class added

---
+++ Models/InstrumentToVerificationTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OLLIMS.Models
+{
+    public class InstrumentToVerificationTests
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public Instrument Instrument { get; set; }
+        public VerificationTest VerificationTest { get; set; }
+    }
+}
