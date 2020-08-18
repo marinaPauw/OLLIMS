72f674d41721f0098dfb0ec8a5bb37922bd6112d
Marina Kriek <pauwmarina@gmail.com>
2020-08-18 13:34:19 +0200

    feat(AddDBContext):DBContext added with Instrument in

---
+++ Data/DBContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using OLLIMS.Models;
+
+namespace OLLIMS.Data
+{
+    public class DBContext:DbContext
+    {
+        public DBContext(DbContextOptions<DBContext> options) : base(options) { }
+        public DbSet<Instrument> Instrument { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Instrument>().HasKey(
+                t => new { t.ID }
+            );
+
+
+        }
+    }
+}
--- Models/Instrument.cs
+++ Models/Instrument.cs
@@ -7,6 +7,7 @@ namespace OLLIMS.Models
 {
     public class Instrument
     {
+        public int ID { get; set; }
         public string Name { get; set; }
         public string Vendor { get; set; }
         public int VerificationSchedule { get; set; }
