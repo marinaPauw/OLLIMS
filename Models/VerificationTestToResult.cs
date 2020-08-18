086869c6976c943a23217d58677a0b0a4e1a4d90
Marina Kriek <pauwmarina@gmail.com>
2020-08-18 12:30:05 +0200

    feat(AddVerificationTestToResult):Class added for relationship

---
+++ Models/VerificationTestToResult.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OLLIMS.Models
+{
+    public class VerificationTestToResult
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public Result Result { get; set; }
+        public VerificationTest VerificationTest{ get; set; }
+    }
+}
