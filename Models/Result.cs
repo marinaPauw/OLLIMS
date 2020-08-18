31d88aba1e55f463781fafbaf799a8bc3d9f7242
Marina Kriek <pauwmarina@gmail.com>
2020-08-18 12:30:43 +0200

    feat(AddClassForResult):Class added

---
+++ Models/Result.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OLLIMS.Models
+{
+    public class Result
+    {
+        public DateTime RunDate { get; set; }
+        public double Value { get; set; }
+        public string Comment { get; set; }
+
+        public bool Deleted = false;
+    }
+}
