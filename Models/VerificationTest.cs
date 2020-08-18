b25ef9218ab2dc9784dc2307fad3951f47c6830f
Marina Kriek <pauwmarina@gmail.com>
2020-08-18 12:29:36 +0200

    feat(AddVerificationTest):Class added

---
+++ Models/VerificationTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OLLIMS.Models
+{
+    public class VerificationTest
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public double MeasurementrangeBottom { get; set; }
+        public double MeasurementrangeTop { get; set; }
+        public string Unit { get; set; }
+        public ICollection<VerificationTestToResult> VerTestToResults { get; set; }
+    }
+}
